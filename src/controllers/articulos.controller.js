7afa21195fba0f164ddf2f49d8293a0cdebd403e
Orlando Sanchez <pdigitalgroup@hotmail.com>
2021-11-14 13:26:06 -0400

    commit

--- src/controllers/articulos.controller.js
+++ src/controllers/articulos.controller.js
@@ -1,15 +1,24 @@
+//import { json } from "express";
 import { getConnection } from "../database/connection";
-import sql from 'mssql';
-
 
 export const getArticulos = async (req, res) => {
     const pool = await getConnection();
     pool.request().input('sCo_NivelStock', 'IA0');
     const result = await pool.request().execute('RepStockArticulos');
     //const result = await pool.request().execute('RepSucursal');
-    res.json(result.recordsets);
-   // res.json(result.recordset[0].co_art[0]);
-    //console.log(result.recordset[0]);
+    let articulos = [];
+    result.recordset.forEach((articulo, index) =>{
+        if (articulo.StockActual > 0){
+            articulos.push({
+                'cod_art': articulo.co_art[0].trim(),
+                'art_des': articulo.art_des[0],
+                'StockActual': articulo.StockActual
+            });       
+        };   
+    }); 
 
+    res.json(articulos);
+    console.log(articulos.length);
+    
 };
 
