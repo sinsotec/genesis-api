257195cd4521a3d46af4ca04986304f78086d978
sinsotec <pdigitalgroup@hotmail.com>
2021-11-14 18:37:44 -0400

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,10 @@
 import sql from 'mssql';
 
 const dbSettings = {
-    server:"caracas1.pidigitalgroup.net", // "149.56.238.94",
+    server: process.env.SERVER,
     database:"DEMOA",
-    user:"profitdigital",
-    password:"634949Tsc..!",
+    user: process.env.USER,
+    password: process.env.PASS,
     options: {
         //encrypt: true,
         trustServerCertificate: true
